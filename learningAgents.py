94b7a282978f35ac770dd4fdb07a1fc1d03837d6
Osher M <oshermaayan@gmail.com>
2018-12-09 19:43:23 +0200

    minor import fixes

--- learningAgents.py
+++ learningAgents.py
@@ -12,6 +12,24 @@
 
 import random,util,time
 
+"""Agent class was copied from game.py"""
+class Agent:
+    """
+    An agent must define a getAction method, but may also define the
+    following methods which will be called if they exist:
+
+    def registerInitialState(self, state): # inspects the starting state
+    """
+    def __init__(self, index=0):
+        self.index = index
+
+    def getAction(self, state):
+        """
+        The Agent will receive a GameState (from either {pacman, capture, sonar}.py) and
+        must return an action from Directions.{North, South, East, West, Stop}
+        """
+        util.raiseNotDefined()
+
 class ValueEstimationAgent(Agent):
     """
       Abstract agent which assigns values to (state,action)
